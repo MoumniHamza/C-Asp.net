5c2af028a64967eb938e119d030cb00212b01c0b
Hamza Moumni <94747@supinfo.com>
2015-09-30 10:24:48 -0700

    This is a simple OOP program that demonstrates the uses of multiple methods in your program to faciliate the readability of your code

---
+++ CalculateAverageOOP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateAverage("my phone", "your phone", 1400, 1250);
+            Console.WriteLine("Type enter to exit");
+            Console.ReadLine();
+        }
+        public static void CalculateAverage(string s1, string s2, double d1,double d2)
+        {
+
+            double average = (d1 + d2) / 2;
+            Console.WriteLine();
+            Console.WriteLine("The average of  " + s1 +" whose value is " + d1 +" and the average of " + s2 +" whose value is " + d2 +" is " + average);
+        }
+        public static void CalculateAverage(double d1, double d2)
+        {
+            double average = (d1 + d2) / 2;
+            Console.WriteLine("The average of :" + d1 + " and :" + d2 + "is equal to " + average);
+        }
+    }
+}
