fdffa08e61df1855b584ef93fdac93a92c1e6914
Hamza Moumni <94747@supinfo.com>
2015-10-05 10:49:06 -0700

    Working with while loops

---
+++ WhileLoop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your target number: ");
+            int target = int.Parse(Console.ReadLine()); //Convert the string to int
+
+            int start = 0;
+            while(start<target)
+            {
+                Console.WriteLine(start);
+                start = start + 2;  // increment the variable by 2
+                Console.ReadLine();
+
+            }
+            Console.WriteLine("Press enter to exit");
+        }
+    }
+}
