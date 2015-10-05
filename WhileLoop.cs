a2ebadc7d48440ea3280a0c5292b5d9828ab9bfc
Hamza Moumni <94747@supinfo.com>
2015-10-05 11:36:53 -0700

    Created a static method then initiated it in the main method to call it

--- WhileLoop.cs
+++ WhileLoop.cs
@@ -10,18 +10,35 @@ namespace ConsoleApplication8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your target number: ");
-            int target = int.Parse(Console.ReadLine()); //Convert the string to int
-
-            int start = 0;
-            while(start<target)
+            string choice = "";
+            do
             {
-                Console.WriteLine(start);
-                start = start + 2;  // increment the variable by 2
-                Console.ReadLine();
+                Console.WriteLine("Please enter your target number: ");
+                int target = int.Parse(Console.ReadLine()); //Convert the string to int
 
-            }
-            Console.WriteLine("Press enter to exit");
+                int start = 0;
+                while (start < target)
+                {
+                    Console.WriteLine(start);
+                    start = start + 2;  // increment the variable by 2
+                    Console.ReadLine();
+
+                }
+                Console.WriteLine("Press enter to exit");
+                
+                do
+                {
+                    Console.WriteLine("Would you like to do it agin? : yes/no : ");
+                    choice = Console.ReadLine().ToUpper();
+                    if (choice != "YES" && choice != "NO")
+                    {
+                        Console.WriteLine("Sorry invalid input try again");
+                        Console.ReadLine();
+
+                    }
+
+                } while (choice != "YES" && choice != "NO");
+            } while (choice == "YES");
         }
     }
 }
---
+++ WorkingWithMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.EvenNumber();
+        }
+
+        public void EvenNumber()
+        {
+            int start = 0;
+            while (start < 30)
+            {
+                Console.WriteLine(start);
+                start = start +2;
+                Console.ReadLine();
+            }
+            Console.WriteLine("Press enter to exit");
+        }
+    }
+}
