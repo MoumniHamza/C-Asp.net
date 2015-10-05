6a55588ec461ac7b3efdab38ceba2e6b8802cfd2
Hamza Moumni <94747@supinfo.com>
2015-10-04 22:18:22 -0700

    This is a simple program that uses the switch statement the break and the goto

--- Switch-CoffeeShopProgram.cs
+++ Switch-CoffeeShopProgram.cs
@@ -7,11 +7,11 @@ using System.Collections.Generic;
         {
         
         int FinalBill = 0;
-        start:
+        start:  // this is a label
         Console.WriteLine("Welcome to our Coffeshop, this is what we serve , what would you like to drink ");
         Console.WriteLine("1- Cappucino, 2- Latte, 3-Expresso");
         Console.WriteLine("When you are ready press the number of your choice");
-        int choice = int.Parse(Console.ReadLine());
+        int choice = int.Parse(Console.ReadLine()); // I convert it to integer
 
         switch(choice)
         {
@@ -28,7 +28,7 @@ using System.Collections.Generic;
                 Console.WriteLine("Sorry {0} is not a valid choice ", choice);
                 break;
         }
-        tryagain:
+        tryagain: // this is a label
         Console.WriteLine(" Would you like to buy something else? yes/no");
         string decision = Console.ReadLine();
 
