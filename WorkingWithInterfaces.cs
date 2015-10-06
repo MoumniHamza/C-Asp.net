a2dbafcd0f1d049dbc885534dc086ff969650f46
Hamza Moumni <94747@supinfo.com>
2015-10-06 12:11:10 -0700

    Explicit Interfaces

---
+++ WorkingWithInterfaces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+
+
+    interface I1
+    {
+        void Print();
+    }
+
+    interface I2
+    {
+        void Print();
+    }
+
+    public class LetsPrint: I1,I2
+    {
+        void I1.Print()
+        {
+            Console.WriteLine("I am interface I1");
+        }
+
+        void I2.Print()
+        {
+            Console.WriteLine("I am interface I2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LetsPrint print = new LetsPrint();
+            ((I1)print).Print();
+            ((I2)print).Print();
+        }
+    }
+}
--- WorkingWithPolymorphism.cs
+++ WorkingWithPolymorphism.cs
@@ -6,65 +6,39 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication14
 {
-
-    class employee
-    {
-        public string firstName = "Hamza";
-        public string lasName = "Moumni";
-
-        public virtual void PrintName()
-        {
-            Console.WriteLine("Your name is " + firstName + " " + lasName);
-            Console.ReadLine();
-            Console.WriteLine("Press enter to exit");
-        }
-    }
-
-    class partimeEmployee : employee
-    {
-        public override void PrintName()
-        {
-            Console.WriteLine("Your name is " + firstName + " " + lasName + " -You are a partime employee");
-            Console.ReadLine();
-            Console.WriteLine("Press enter to exit");
-        }
-    }
-
-    class fulltimeEmployee : employee
-    {
-        public override void PrintName()
-        {
-            Console.WriteLine("Your name is " + firstName + " " + lasName + " -You are a fulltime employee");
-            Console.ReadLine();
-            Console.WriteLine("Press enter to exit");
-        }
-    }
-
-    class temporaryEmployee : employee
+    class student
 
     {
-        public override void PrintName()
+        string _name;
+        string _id;
+        string _score;
+        
+        public void getName(string name)
         {
-            Console.WriteLine("Your name is " + firstName + " " + lasName + " -You are a temporary employee");
-            Console.ReadLine();
-            Console.WriteLine("Press enter to exit");
+            if(name == "")
+            {
+                throw new Exception("Please enter a valid name");
+            }
+            this._name = name;
+        }
+
+        public string SetName(string name)
+        {
+            return this._name;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Your name is : {0}", SetName("hamza"));
         }
     }
+   
     class Program
     {
         static void Main(string[] args)
         {
-            employee[] name = new employee[4];
-            name[0] = new employee();
-            name[1] = new fulltimeEmployee();
-            name[2] = new partimeEmployee();
-            name[3] = new temporaryEmployee();
-
-            foreach (employee e in name)
-            {
-                e.PrintName();
-            }
-
+            student name = new student();
+            name.PrintName();
 
 
         }
