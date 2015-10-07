c56dbe457846a5cc916e67aa06b8b9b054e60ca5
Hamza Moumni <94747@supinfo.com>
2015-10-07 10:47:26 -0700

    Working with generics

---
+++ Generics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isequal = Equality.Equal<string>("A", "2");
+            if(isequal)
+            {
+                Console.WriteLine("It is equal");
+
+            }
+            else
+            {
+                Console.WriteLine("It is not equal");
+            }
+            Console.ReadLine();
+            Console.WriteLine("Press enter to exit");
+        }
+
+    class Equality
+        {
+            public static bool Equal<T>(T number, T number1)
+            {
+                return number.Equals(number1);
+            }
+        }
+    }
+}
--- PowerOfDelegates.cs
+++ PowerOfDelegates.cs
@@ -3,53 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApplication16
 {
-    class employee
-    {
-        public string name { get; set; }
-        public int id { get; set; }
-        public int salary { get; set; }
-        public int experience { get; set; }
-        public static void promotion(List<employee> employed, AmIPromoted promoted)
-        {
-            foreach (employee e in employed)
-            {
-                if (promoted(e))
-                {
-                    Console.WriteLine(e.name);
-                }
-            }
-        }
-    }
-
-    delegate bool AmIPromoted(employee emp);
+   
     class Program
     {
         static void Main(string[] args)
         {
-            List<employee> employees = new List<employee>();
-            employees.Add(new employee() { name = "Hamza", experience = 5, id = 101, salary = 7000 });
-            employees.Add(new employee() { name = "Bob", experience = 14, id = 102, salary = 71000 });
-            employees.Add(new employee() { name = "ahmed", experience = 2, id = 103, salary = 2000 });
-            employees.Add(new employee() { name = "lina", experience = 4, id = 104, salary = 5000 });
-
-            AmIPromoted promoted = new AmIPromoted(promote);
-
-            employee.promotion(employees, promoted);
+            StreamReader stream = new StreamReader(@"C:\Users\Mohamedhamza\Desktop\hamza.txt");
+            Console.WriteLine(stream.ReadToEnd());
+            Console.ReadLine();
+            Console.WriteLine("Press enter to exit");
+            stream.Close();
+           
         }
-        public static bool promote(employee emp)
-        {
-            if (emp.experience >= 5)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        
 
     }
 }
